25ff23b033278d20fea8ef2b61d1e15cf8c98a6a
Nathan Freeman <nathanlfreeman@gmail.com>
2022-10-20 02:23:28 -0400

    update allowed hosts

--- music_api/music_api/settings.py
+++ music_api/music_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'young-savannah-30515.herokuapp.com', 'audiophile-back.onrender.com']
+ALLOWED_HOSTS = ['localhost', 'young-savannah-30515.herokuapp.com', '.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
